3e6bbb159fb96fd5fade3ee489e3b00de9ccdd6a
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-08-11 15:48:29 -0500

    Dictionary Excerises are Done

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,28 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            while (true)
+            {
+                Console.WriteLine("Enter Student Name: ");
+                string studentName = Console.ReadLine();
+                if (studentName == "")
+                {
+                    Console.WriteLine("input ended.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Student ID: ");
+                    int studentID = int.Parse(Console.ReadLine());
+                    students.Add(studentName, studentID);
+
+                }
+            }
+            foreach (string studentName in students.Keys)
+            {
+                Console.WriteLine($"{studentName}: {students[studentName]}");
+            }
+        }                
     }
 }
